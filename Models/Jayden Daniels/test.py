4f54a7462c1b4e65cd099107f3846136ce7d3838
DylanLiesenfelt <dylanjames.code@gmail.com>
2024-11-06 01:38:29 -0500

    changes needed

---
+++ Models/Jayden Daniels/test.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import Lasso
+from sklearn.metrics import mean_absolute_percentage_error, mean_squared_error, r2_score
+
+def read_data(path):
+    return pd.read_csv(path).dropna()
+
+def split_data():
+    return
+
+def make_model():
+    return
+
+def train_model():
+    return
+
+def eval_model():
+    return
+
+def get_defense_stats():
+    return
+
+def get_qb_carrer_avg_stats():
+    return
+
+def get_qb_MA3_stats():
+    return
+
+def make_inputs():
+    return
+
+def prediction():
+    return
+
+if __name__ == "__main__":
+    prediction()
